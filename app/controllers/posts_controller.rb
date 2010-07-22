3d51b7e0b74b2f0b62b916a56a8a81e5bb378dd6
Lee Martin <leepaulmartin@gmail.com>
2010-07-22 10:36:19 -0700

    added some basic fb posting tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,28 @@
 class PostsController < ApplicationController
   
   def new
+    
+    #puts Facebook.new(session[:facebook]).class.get('/me')
+    
   end
 
   def create
+    
+=begin
+
+response = Facebook.new(session[:facebook]).class.post('/me/feed', 
+  :query => {
+    :message => "testing social download",
+    #:picture => @product.image("compact"),
+    :link => root_url,
+    :name => "social download"
+    #:caption
+    #:description
+  }
+)
+
+=end    
+    
   end
 
 end
