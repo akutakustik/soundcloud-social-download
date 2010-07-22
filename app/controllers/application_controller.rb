8781a3589590416e6e0261faeeeea92199d70999
Lee Martin <leepaulmartin@gmail.com>
2010-07-22 13:25:49 -0700

    added config variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,18 @@ class ApplicationController < ActionController::Base
   
   def consumer(service)
     if service == "twitter"
-      @consumer ||= OAuth::Consumer.new("D9tonxAqgH73v3h3bIPNTA", "hfcEuzoHRSKDVzaPkxQZHrX3bDjOHnNtaGIEnMw", :site => 'https://twitter.com')
+      @consumer ||= OAuth::Consumer.new(config['twitter']['key'], config['twitter']['secret'], :site => 'https://twitter.com')
     elsif service == "soundcloud"
-      @consumer ||= OAuth::Consumer.new("J8aFr3h5xyOSkYxsJMYXQ", "cCysasMDFSo8ErnCaAJQzpl8LUrkoziAVSTvzOhJWc", :site => 'http://api.soundcloud.com')
+      @consumer ||= OAuth::Consumer.new(config['soundcloud']['key'], config['soundcloud']['secret'], :site => 'http://api.soundcloud.com')
     end
   end
   
   def client
-    @client ||= OAuth2::Client.new("130178800328203", "82933a3c53aaf5cb027ec0ee6ced6c87", :site => 'https://graph.facebook.com')
+    @client ||= OAuth2::Client.new(config['facebook']['app_id'], config['facebook']['secret'], :site => 'https://graph.facebook.com')
   end
   
   def config
-    # config = YAML.load(File.open(RAILS_ROOT + '/config/facebook_auth.yml').read)[RAILS_ENV]
-    # config['app_id'], config['secret'], :site => 'https://graph.facebook.com'
+    config = YAML.load(File.open(RAILS_ROOT + '/config/auth.yml').read) #[RAILS_ENV]
   end
   
 end
