10b78b4d199313656279bf104056382e890faad3
Lee Martin <leepaulmartin@gmail.com>
2010-07-24 11:52:03 -0700

    created index action and added twitter counter

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,21 @@
 jQuery(document).ready(function($) {
   $('a[rel*=facebox]').facebox({
-		opacity			 : 0.5,
-		loadingImage 	 : '/images/loading.gif'
+		opacity			 : 0.5
 	}) 
-})
+});
+
+function count(maxLength) {
+
+	var lngCount = $("#post_text").val().length;
+	
+	currentLength = maxLength - lngCount;
+	
+	if (lngCount > maxLength) {
+		$("#count").html("<span style='color:red;'>" + currentLength + "</span>");
+		$('#post_submit').attr('disabled', 'disabled');
+	} else {
+		$("#count").html(currentLength);
+		$('#post_submit').removeAttr('disabled');
+	}
+	
+}
