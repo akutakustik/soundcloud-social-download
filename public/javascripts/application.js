9ffd49f7303fe998a1107acf8556e611d92eee1e
Lee Martin <leepaulmartin@gmail.com>
2010-07-24 09:23:33 -0700

    added cancel link, removed cancel image

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function($) {
   $('a[rel*=facebox]').facebox({
 		opacity			 : 0.5,
-		loadImage 	 : '/images/loading.gif',
-		closeImage   : '/images/closelabel.gif'
+		loadImage 	 : '/images/loading.gif'
 	}) 
 })
