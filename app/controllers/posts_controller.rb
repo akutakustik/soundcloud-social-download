c657f990127e32a86e3d95d51707e1ca66950315
Lee Martin <leepaulmartin@gmail.com>
2010-07-26 13:18:05 -0700

    added location

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
     if session[:download]
       track_url = oauth("soundcloud", Settings.token, Settings.secret).get("/resolve?url=#{Settings.config["track"]}")["location"]
       #logger.info track_url
-      location = oauth("soundcloud", Settings.token, Settings.secret).get("#{track_url}/download") #["location"]
+      location = oauth("soundcloud", Settings.token, Settings.secret).get("#{track_url}/download")["location"]
       logger.info location
     else
       redirect_to root_path
