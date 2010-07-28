361db7213cf5122e1d215e062c80ea3b6dd6d4bb
Lee Martin <leepaulmartin@gmail.com>
2010-07-27 20:13:15 -0700

    fixed comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,9 +26,7 @@ class SessionsController < ApplicationController
       
       access_token = client.web_server.get_access_token(params[:code], :redirect_uri => oauth_callback_url("facebook"))
       
-      #result = Facebook.new(access_token.token).class.get('/me')
-      
-      result = access_token.get('/me') # does this work?
+      result = access_token.get('/me') # does this work? if not Facebook.new(access_token.token).class.get('/me')
       
       session[:user] = {:service => "facebook", :token => access_token.token, :name => result["name"], :picture => "https://graph.facebook.com/#{result["id"]}/picture"}
       
