c0f6e2a7617817f159094776f289aec4a80eda09
Lee Martin <leepaulmartin@gmail.com>
2010-07-27 20:10:27 -0700

    cleaned

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,18 +10,7 @@ class PostsController < ApplicationController
     
     if @post["service"] == "facebook"
       
-      # does it work automatically with share meta?
-      
-      response = Facebook.new(session[:user][:token]).class.post('/me/feed', 
-        :query => {
-          :message => @post["text"],
-          :link => root_url
-          #:picture => "#{root_url}images/artwork.jpg",
-          #:name => "social download" # title of page
-          #:caption => "social-download.com", # caption of title
-          #:description => "social post for a free download" # description below caption
-        }
-      )
+      response = Facebook.new(session[:user][:token]).class.post('/me/feed', :query => {:message => @post["text"], :link => root_url})
       
     elsif @post["service"] == "twitter"
       
@@ -36,8 +25,6 @@ class PostsController < ApplicationController
   end
   
   def download
-    
-    # add fail safe?
     
     if session[:download]
       location = oauth("soundcloud", Settings.token, Settings.secret).get("#{SETTINGS["track"]}/download")["location"]
