95e96f4ade340c2dea7f8dfd130921624b4ab659
Lee Martin <leepaulmartin@gmail.com>
2010-09-02 11:24:13 +0100

    added soundcloud login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
 
   def index
     
-    #if session[:user] && session[:user][:service] == "soundcloud"
+    if session[:user] && session[:user][:service] == "soundcloud"
       @users = User.all
-    #else
-      #redirect_to login_path("soundcloud")
-    #end
+    else
+      redirect_to login_path("soundcloud")
+    end
     
   end
   
